87eab12c1cf80b3c7f421ed5183e4f6f4cd9870f
Jose Carlos <arzajose@gmail.com>
2026-08-12 10:02:50 -0600

    feat: connect to MongoDB with mongoose

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
@@ -9,6 +10,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('News Explorer backend funcionando');
 });
+
+mongoose.connect('mongodb://127.0.0.1:27017/news-explorer')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
